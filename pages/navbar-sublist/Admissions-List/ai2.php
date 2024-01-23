47edfed319793138e23a1d782e6b79412b6107ba
PinQiH <cassie34131@gmail.com>
2024-01-23 10:39:07 +0800

    新增ai2

---
+++ pages/navbar-sublist/Admissions-List/ai2.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../../../headConfig.php'; ?>
+
+    <title>修業規定</title>
+</head>
+
+<body>
+    <div id="header"></div>
+    <div class="wrap">
+        
+        <div class="href">
+            <ol>
+                <li class="a1"><a href="./index.php">首頁</a></li>
+                <li class="a2"><a href="./pages/navbar-headlist/admissions.php">招生資訊</a></li>
+                <li class="a3"><a href="./pages/navbar-sublist/Admissions-List/aiDegree.php">AI跨領域技優專班</a></li>
+                <li class="a4"><a href="./pages/navbar-sublist/Admissions-List/ai2.php">修業規定</a></li>
+            </ol>
+        </div>
+
+        <div class="main">
+            <div class="title">
+                <h2>修業規定</h2><br><br>
+            </div>
+
+            <div class="content">
+                <div class="container1">
+                    <table class="table" cellpadding="0" cellspacing="0">
+                        <tbody>
+                            <tr class="row header">
+                                <th class="cell" id="year">畢業條件</th>
+                                <th class="cell" id="thing">內容</th>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">應修學分數</td>
+                                <td class="cell">135</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">英語檢定</td>
+                                <td class="cell">如下表</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">專業證照</td>
+                                <td class="cell">至少需取得本系專業認證項目暨認證標準或視同認證項目乙項者之積分達6分，始得畢業。</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">實務專題</td>
+                                <td class="cell">必修(一學年)</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">專業實習</td>
+                                <td class="cell">必修(一學年；18學分)</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">輔系</td>
+                                <td class="cell">修習他系之選修學分達20學分</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">第二專長</td>
+                                <td class="cell">修習他系之選修學分</td>
+                            </tr>
+                        </tbody>
+                    </table><br><br>
+
+                    <table class="table" cellpadding="0" cellspacing="0">
+                        <tbody>
+                            <tr class="row header">
+                                <th class="cell" id="year">項目</th>
+                                <th class="cell" id="thing" colspan="2">通過標準</th>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell" id="year">多益測驗<br>（NEW TOEIC）</td>
+                                <td class="cell">450分以上</td>
+                                <td class="cell">聽力須達110分以上。<br>閱讀須達115分以上。</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">新制多益普及英語測驗<br>（TOEIC Bridge）</td>
+                                <td class="cell">70分以上</td>
+                                <td class="cell">聽力須達30分以上。<br>閱讀須達40分以上。</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">全民英檢<br>（GEPT）</td>
+                                <td class="cell">初級複試以上</td>
+                                <td class="cell">讀、聽力成績總和須達180分以上<br>寫作、口說成績均須達80分以上</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">全民網路英語能力檢定<br>（NETPAW）</td>
+                                <td class="cell">初級以上</td>
+                                <td class="cell">依全民網路英語能力檢定之通過規定</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">通用國際英文能力分級檢定<br>（G-TELP）</td>
+                                <td class="cell">level 4 以上</td>
+                                <td class="cell">依通用國際英文能力分級檢定之通過規定。</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">全球英檢<br>（GET）</td>
+                                <td class="cell">A2級以上</td>
+                                <td class="cell">聽力、閱讀測驗成績總和須達150分，且任一項成績不得低於70分。</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">劍橋大學國際商務英語能力測驗<br>（BULATS）</td>
+                                <td class="cell">A2級以上</td>
+                                <td class="cell">總分26分以上。</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">劍橋領思職場英語檢測<br>（Linguaskill business）</td>
+                                <td class="cell">A2級以上</td>
+                                <td class="cell">120分以上。</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">劍橋領思實用英語檢測<br>（Linguaskill general）</td>
+                                <td class="cell">A2級以上</td>
+                                <td class="cell">120分以上。</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">國際英語認證<br>（ILTEA）</td>
+                                <td class="cell">A2級以上</td>
+                                <td class="cell">聽力、閱讀測驗成績總和須達140分，且任一項成績不得低於60分。</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">外語能力測驗<br>（FLPT-English）</td>
+                                <td class="cell">A2級以上</td>
+                                <td class="cell">三項筆試總分130分以上，口試級分S-1+。</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">大學院校英語能力測驗<br>（CSEPT）</td>
+                                <td class="cell">A2級以上</td>
+                                <td class="cell">第一級145分以上<br>第二級150分以上<br>依大學院校英語能力測驗之通過規定。</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">劍橋大學英語能力認證分級測驗<br>（Cambridge Main Suite）</td>
+                                <td class="cell">A2級以上</td>
+                                <td class="cell">KET-A2: 126分以上</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">托福紙筆測驗<br>（TOEFL ITP）</td>
+                                <td class="cell">A2級以上</td>
+                                <td class="cell">400分以上（聽力須達38分以上；文法結構須達32分以上；閱讀須達31分以上）</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">托福網路測驗<br>（TOEFL IBT）</td>
+                                <td class="cell">A2級以上</td>
+                                <td class="cell">24分以上（聽力須達3分以上；閱讀須達4分以上；口說須達10分以上；寫作須達7分以上）</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">國際英語語文測驗<br>（雅思IELTS）</td>
+                                <td class="cell">A2級以上</td>
+                                <td class="cell">3.5級分以上</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="cell">專業英文詞彙能力國際認證<br>（PVQC）</td>
+                                <td class="cell">A2級以上</td>
+                                <td class="cell">Specialist Tier 3 + Spelling Tier 1 (含)以上</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer"></div>
+</body>
+
+</html>
