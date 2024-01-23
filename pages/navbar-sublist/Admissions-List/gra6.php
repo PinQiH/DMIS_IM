dbc6a298dc0700decb2ccbab2d83d29ed45fefc6
PinQiH <cassie34131@gmail.com>
2024-01-23 10:23:47 +0800

    新增gra6

---
+++ pages/navbar-sublist/Admissions-List/gra6.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../../../headConfig.php'; ?>
+
+    <title>學習輔助資源(Hỗ Trợ Học Tập)</title>
+</head>
+
+<body>
+    <div id="header"></div>
+    <div class="wrap">
+
+        <div class="href">
+            <ol>
+                <li class="a1"><a href="./index.php">首頁</a></li>
+                <li class="a2"><a href="./pages/navbar-headlist/admissions.php">招生資訊</a></li>
+                <li class="a3"><a href="./pages/navbar-sublist/Admissions-List/masterDegree.php">AI加值智慧製造專班</a></li>
+                <li class="a4"><a href="./pages/navbar-sublist/Admissions-List/gra6.php">學習輔助資源(Hỗ Trợ Học Tập)</a></li>
+            </ol>
+        </div>
+
+        <div class="main">
+            <div class="photo">
+                <img src="./assets/images/德明財經科技大學banner02-2023.11.06.jpg" class="d-block w-100 " alt="...">
+            </div><br><br>
+
+            <div class="title">
+                <h2>學習輔助資源(Hỗ Trợ Học Tập)</h2><br><br>
+            </div>
+
+            <div class="content">
+                <div class="container1">
+                    <h5 id="text">用越語學中文檢定(油漆式速記訓練系統)</h5><br>
+
+                    <div class="detail">
+                        <ul>
+                            <li>連結網址：<a href="http://pmm.zct.com.tw/seat/">http://pmm.zct.com.tw/seat/</a></li>
+                            <li>初級檢定密碼：takming_gv</li>
+                            <li>中級檢定密碼：takming_gv2</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer"></div>
+</body>
+
+</html>
