6feba0f86cf9598802d47c3de680de3cd48b86f4
PinQiH <cassie34131@gmail.com>
2024-01-23 10:17:53 +0800

    新增gra4

---
+++ pages/navbar-sublist/Admissions-List/gra4.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../../../headConfig.php'; ?>
+
+    <title>課程基準表(Chương Trình Học)</title>
+</head>
+
+<body>
+    <div id="header"></div>
+    <div class="wrap">
+
+        <div class="href">
+            <ol>
+                <li class="a1"><a href="./index.php">首頁</a></li>
+                <li class="a2"><a href="./pages/navbar-headlist/admissions.php">招生資訊</a></li>
+                <li class="a3"><a href="./pages/navbar-sublist/Admissions-List/masterDegree.php">AI加值智慧製造專班</a></li>
+                <li class="a4"><a href="./pages/navbar-sublist/Admissions-List/gra4.php">課程基準表(Chương Trình Học)</a></li>
+            </ol>
+        </div>
+
+        <div class="main">
+            <div class="photo">
+                <img src="./assets/images/德明財經科技大學banner02-2023.11.06.jpg" class="d-block w-100 " alt="...">
+            </div><br><br>
+
+            <div class="title">
+                <h2>課程基準表(Chương Trình Học)</h2><br><br>
+            </div>
+
+            <div class="content">
+                <div class="container">
+                    <div class="table-responsive">
+                        <table class="table custom-table">
+                            <tr>
+                                <td><a class="dropdown-item" href="https://misweb.takming.edu.tw/var/file/31/1031/img/107063890.pdf">111課程基準表</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><a class="dropdown-item" href="https://misweb.takming.edu.tw/var/file/31/1031/img/876724276.pdf">112課程基準表</a>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer"></div>
+</body>
+
+</html>
