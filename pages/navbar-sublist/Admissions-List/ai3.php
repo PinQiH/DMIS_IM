8bd44eed147d7c0ce009d014c9eac3fccdcf01a8
PinQiH <cassie34131@gmail.com>
2024-01-23 10:41:03 +0800

    新增ai3

---
+++ pages/navbar-sublist/Admissions-List/ai3.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../../../headConfig.php'; ?>
+
+    <title>課程地圖</title>
+</head>
+
+<body>
+    <div id="header"></div>
+    <div class="wrap">
+
+        <div class="href">
+            <ol>
+                <li class="a1"><a href="./index.php">首頁</a></li>
+                <li class="a2"><a href="./pages/navbar-headlist/admissions.php">招生資訊</a></li>
+                <li class="a3"><a href="./pages/navbar-sublist/Admissions-List/aiDegree.php">AI跨領域技優專班</a></li>
+                <li class="a4"><a href="./pages/navbar-sublist/Admissions-List/ai3.php">課程地圖</a></li>
+            </ol>
+        </div>
+
+        <div class="main">
+            <div class="photo">
+                <img src="./assets/images/315116025986.jpg" class="d-block w-100 " alt="...">
+            </div><br><br>
+        </div>
+    </div>
+    <div id="footer"></div>
+</body>
+
+</html>
