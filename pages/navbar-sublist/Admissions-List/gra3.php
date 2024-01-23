e1903a84e9630444fb5eabcf4186cc511f82ffb3
PinQiH <cassie34131@gmail.com>
2024-01-23 10:15:14 +0800

    新增gra3

---
+++ pages/navbar-sublist/Admissions-List/gra3.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../../../headConfig.php'; ?>
+
+    <title>課程規劃(Kế Hoạch Giảng Dạy)</title>
+</head>
+
+<body>
+    <div id="header"></div>
+    <div class="wrap">
+
+        <div class="href">
+            <ol>
+                <li class="a1"><a href="./index.php">首頁</a></li>
+                <li class="a2"><a href="./pages/navbar-headlist/admissions.php">招生資訊</a></li>
+                <li class="a3"><a href="./pages/navbar-sublist/Admissions-List/masterDegree.php">AI加值智慧製造專班</a></li>
+                <li class="a4"><a href="./pages/navbar-sublist/Admissions-List/gra3.php">課程規劃(Kế Hoạch Giảng Dạy)</a></li>
+            </ol>
+        </div>
+
+        <div class="main">
+            <div class="photo">
+                <img src="./assets/images/德明財經科技大學banner02-2023.11.06.jpg" class="d-block w-100 " alt="...">
+            </div><br><br>
+
+            <div class="title">
+                <h2>課程規劃(Kế Hoạch Giảng Dạy)</h2><br><br>
+            </div>
+
+            <div class="content">
+                <div class="container1">
+                    <h6 id="text">本國際專班的教育目標為「以AI技術優化生產製程與提高產品附加價值」，希望透過技職體系人才培育與廠商合作將AI實務應用於檢測與加工等層面，建立一套有效的智慧製造系統解決方案，進而協助國內製造業者提升產線效率與機台附加價值，促進產業智慧製造能力升級，為我國製造業帶來跳躍性成長。</h6>
+                    <h6 id="text">本計畫之執行，有助於本國的資訊與製造產業對越南籍資訊與製造專業人才之培養，解決台商在越南的相關人才不足問題。本專班預期成效如下：</h6><br><br>
+
+                    <div class="detail">
+                        <ul>
+                            <li>本專班可以培育瞭解台灣的外籍人才，為二國台商企業提供大量人才庫，專班學生畢業後即具備直接進到企業在兩國的工廠裡工作的能力，成為台灣企業在越南的人才尖兵，厚植台灣廠商國際競爭力。</li>
+                            <li>藉由專班學生在台期間的文化交流，讓台灣成為他們心目中的第二故鄉。專班學生將專業技能與知識帶回越南後，也可以協助家鄉發展！加深越南民眾對於台灣的好感以進而拓展外交，成為台灣的國際夥伴。</li>
+                            <li>讓企業提早至校園內參與人才培育的過程，讓學生得以在就業前，即能夠對於資訊與製造產業的實務有更完整的認知，以利未來的生涯規劃，並為就業提早做準備。</li>
+                            <li>校內老師透過理論的教授與實例的舉證，將理論實務化，結合業界實作課程與實習的帶領，有助於將實務理論化。配合雙師制與實習，將理論與實務結合為一，提昇學生學習成效，培養整合性之專業人才。</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer"></div>
+</body>
+
+</html>
