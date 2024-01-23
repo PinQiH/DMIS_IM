7c68f1014752c56ff08817e3fe42bec1bc3947de
PinQiH <cassie34131@gmail.com>
2024-01-23 10:19:52 +0800

    新增gra5

---
+++ pages/navbar-sublist/Admissions-List/gra5.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../../../headConfig.php'; ?>
+
+    <title>教育部境外生諮詢專線(Bộ giáo dục Đường dây nóng)</title>
+</head>
+
+<body>
+    <div id="header"></div>
+    <div class="wrap">
+
+        <div class="href">
+            <ol>
+                <li class="a1"><a href="./index.php">首頁</a></li>
+                <li class="a2"><a href="./pages/navbar-headlist/admissions.php">招生資訊</a></li>
+                <li class="a3"><a href="./pages/navbar-sublist/Admissions-List/masterDegree.php">AI加值智慧製造專班</a></li>
+                <li class="a4"><a href="./pages/navbar-sublist/Admissions-List/gra5.php">教育部境外生諮詢專線(Bộ giáo dục Đường dây nóng)</a></li>
+            </ol>
+        </div>
+
+        <div class="main">
+            <div class="photo">
+                <img src="./assets/images/德明財經科技大學banner02-2023.11.06.jpg" class="d-block w-100 " alt="...">
+            </div><br><br>
+
+            <div class="title">
+                <h2>教育部境外生諮詢專線(Bộ giáo dục Đường dây nóng)</h2><br><br>
+            </div>
+
+            <table class="table">
+                <tr class="row header">
+                    <th class="cell">教育部境外生諮詢專線(Bộ giáo dục Đường dây nóng)</th>
+                </tr>
+                <tr class="row" onclick="window.location='https://misweb.takming.edu.tw/var/file/31/1031/img/370/341348431.pdf';" style="cursor: pointer;">
+                    <td class="cell">教育部境外生諮詢專線</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div id="footer"></div>
+</body>
+
+</html>
